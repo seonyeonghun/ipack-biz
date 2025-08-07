fcfab61d76e1afdf2f00b74e96431e8660b5b638
seonyeonghun <sprax@naver.com>
2025-08-07 18:47:34 +0900

    헤더 - 레이아웃 위젯 작성

---
+++ client/src/widgets/layout/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="mx-auto w-360 flex justify-between items-center">
+        <div className='logo'>대한영농 (주)아이팩</div>
+        <nav>
+          <ul className="flex gap-2">
+            <li>처음으로</li>
+            <li>회사소개</li>
+            <li>사업분야</li>
+            <li>고객센터</li>
+          </ul>
+        </nav>
+        <ul className="flex gap-2">
+          <li><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+          </svg>
+          </li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+          </svg>
+          </li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+
+          </li>
+        </ul>
+        <button className="btn btn-soft btn-success">로그인</button>
+      </div>
+    </header>
+  )
+}
