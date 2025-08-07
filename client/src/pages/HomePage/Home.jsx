532ae91643cc3e787a168c47419b2188de4fa771
seonyeonghun <sprax@naver.com>
2025-08-07 18:51:49 +0900

    홈(HOME) 라우트에 아이템필터 경로 추가

--- client/src/pages/HomePage/Home.jsx
+++ client/src/pages/HomePage/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Header } from '../../widgets/layout/Header/Header'
 import DaisyCarousel from '../../widgets/Carousel/DaisyCarousel'
-
+import ItemsFilter from '../../features/CategoryFilter/ItemsFilter'
 export const Home = () => {
   return (
     <>
