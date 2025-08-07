62080d025a0afd177dc88180a848ae9ac2193cf5
seonyeonghun <sprax@naver.com>
2025-08-07 18:51:23 +0900

    아이템 필터 기능추가

---
+++ client/src/features/CategoryFilter/ItemsFilter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ItemsFilter() {
+  return (
+    <div>
+        <h2 className="text-lg text-center text-bold my-5">취급분야</h2>
+        <ul className="flex justify-center gap-10">
+            <li>피트모스&middot;펄라이트</li>
+            <li>포장용기</li>
+            <li>밀폐그릇</li>
+            <li>해면기&middot;선별기</li>
+        </ul>
+    </div>
+  )
+}
+
+export default ItemsFilter
